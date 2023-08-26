142606d232a8ad5a083b5354e027a3ad64a26a92
Phomolo <pphiri1012x@gmail.com>
2023-08-27 00:40:29 +0200

    Add landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import Hero from "../components/HeroSection/Hero";
+import Navbar from "../components/Navbar/Navbar";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      DigiDonut Studio
+    </div>
+  );
+}
