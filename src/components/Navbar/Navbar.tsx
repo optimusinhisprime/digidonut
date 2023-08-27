01555c98f5cbe626d81ca97b55a4385d2d3ad136
Phomolo <pphiri1012x@gmail.com>
2023-08-27 22:18:33 +0200

    Add nav items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ContactUsModal from "./ContactUsModal";
+import { Flex } from "@chakra-ui/layout";
+import { Avatar, Image } from "@chakra-ui/react";
+import SiteLogo from "../../images/site-logo.png";
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <Link to="/">Home</Link>
+    <Flex p="5" mb="10" justifyContent="space-between" alignItems="center">
+      <Flex alignItems="center" w="40%" justifyContent="space-evenly">
+        <Image
+          onClick={() => {
+            navigate("/");
+          }}
+          cursor="pointer"
+          boxSize="50px"
+          objectFit="cover"
+          src={SiteLogo}
+          alt="Dan Abramov"
+        />
+        <Link to="/">Home</Link>
+        <Link to="/#why-us">Why Us</Link>
+        <Link to="/#services">Service</Link>
+        <Link to="/#faq">FAQS</Link>
+      </Flex>
+
       <ContactUsModal />
-    </div>
+    </Flex>
   );
 }
