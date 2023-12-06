fa304d2c2ac56d8c15e2fcdd9aaa59a36a4d3916
Agent 007 <pphiri1012x@gmail.com>
2023-12-06 23:11:12 +0200

    Add section navigation to the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,44 +1,76 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-scroll";
 import ContactUsModal from "./ContactUsModal";
 import { Flex } from "@chakra-ui/layout";
 import { Image, Text } from "@chakra-ui/react";
 import SiteLogo from "../../images/site-logo.png";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsSticky(scrollPosition > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Flex
       p="5"
-      mb={["5", "10"]}
+      bgColor={"#01110A"}
+      color={"#F2F4F3"}
       justifyContent="space-between"
       alignItems="center"
+      position={isSticky ? "fixed" : "relative"}
+      top="0"
+      left="0"
+      right="0"
+      zIndex="1000"
+      transition="background-color 0.3s, color 0.3s"
     >
-      <Flex
-        onClick={() => {
-          navigate("/");
-        }}
-        alignItems="center"
-        cursor="pointer"
-      >
+      <Flex alignItems="center" cursor="pointer">
         <Image
           boxSize="50px"
           objectFit="cover"
           src={SiteLogo}
-          alt="Dan Abramov"
+          alt="DigiDonut Studio"
         />
-        <Text fontSize="large" fontWeight="bold" ml="2">
+        <Text
+          onClick={() => {
+            navigate("/");
+          }}
+          fontSize="large"
+          fontWeight="bold"
+          ml="2"
+        >
           DigiDonut Studio
         </Text>
       </Flex>
 
       <Flex w="40%" justifyContent="space-evenly">
-        <Link to="/">
-          <Text fontWeight="semibold">Home</Text>
+        <Link to="hero" smooth={true} duration={500}>
+          <Text cursor="pointer" fontWeight="semibold">
+            Home
+          </Text>
         </Link>
-        <Link to="/#why-us">Why Us</Link>
-        <Link to="/#services">Service</Link>
-        <Link to="/#faq">FAQS</Link>
+        <Link to="why-us" smooth={true} duration={500}>
+          <Text cursor="pointer">Why Us</Text>
+        </Link>
+        <Link to="services" smooth={true} duration={500}>
+          <Text cursor="pointer">Services</Text>
+        </Link>
+        <Link to="faq" smooth={true} duration={500}>
+          <Text cursor="pointer">FAQs</Text>
+        </Link>
       </Flex>
       <ContactUsModal />
     </Flex>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,19 @@
+import FrequentlyAskedQuestions from "../components/Faqs/Faqs";
+import Footer from "../components/Footer/Footer";
 import Hero from "../components/HeroSection/Hero";
 import Navbar from "../components/Navbar/Navbar";
+import ServicesProvided from "../components/ServicesProvided/ServicesProvided";
+import WhyUs from "../components/WhyUs/WhyUs";
 
 export default function LandingPage() {
   return (
     <div>
       <Navbar />
-      <Hero />
-      DigiDonut Studio
+      <Hero id="hero" />
+      <WhyUs id="why-us" />
+      <ServicesProvided id="services" />
+      <FrequentlyAskedQuestions id="faq" />
+      <Footer />
     </div>
   );
 }
